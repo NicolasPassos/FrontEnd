eb43542c5ef16553351022420c2bf3e5cdf4daf3
Nicolas Passos <nicolasduart21@gmail.com>
2023-10-09 21:44:19 -0400

    Adicionando validação de login

---
+++ bancoDados.py
@@ -0,0 +1,12 @@
+import sqlite3
+
+def validarLogin(usuario: str,senha: str):
+    login_validation = False
+    with sqlite3.Connection(r'bancoDaora.db') as conexao:
+        cursor = conexao.cursor()
+        cursor.execute(f'select * from usuarios where usuario = "{usuario}" and senha = "{senha}"')
+        consulta = cursor.fetchall()
+        if len(consulta) == 1:
+            login_validation = True
+        cursor.close()
+        return login_validation
--- telaLogin.py
+++ telaLogin.py
@@ -1,40 +1,59 @@
 import flet as ft
+from bancoDados import validarLogin
+
+# Padronização do tamanho dos campos
+_with_input = 250
+_size_text = 20
+_with_button = 250
+_height_button = 50
 
 def main(page: ft.Page):
+    # Definindo orientação da página
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    # Criando coluna e sua orientação
     coluna = ft.Column()
     coluna.horizontal_alignment = ft.CrossAxisAlignment.CENTER
 
+    # criando as funções
+    def realizarLogin(e):
+        if validarLogin(usuario=login.value,senha=senha.value) == True:
+            popUp.title = ft.Text(f'Bem-vindo {login.value}!')   
+        else:
+            popUp.title = ft.Text(f'Credenciais inválidas.')
 
-    texto = ft.Text(value="Só um teste bóia", text_align= ft.TextAlign.CENTER)
-    login = ft.TextField(label='Insira seu login', width=200,text_align=ft.TextAlign.CENTER)
-    senha = ft.TextField(label='Insira sua senha',password=True, width=200,text_align=ft.TextAlign.CENTER)
+        login.value = ''
+        senha.value = ''
 
-    resposta = ft.AlertDialog(title=ft.Text('Login realizado com sucesso!'),content=texto,on_dismiss='')
+        page.dialog = popUp
+        popUp.open = True
+        page.update()
     
-    def fecharPopUp(e):
-        resposta.open = False
+    def esqueciSenha(e):
+        popUp.title = ft.Text('Problema teu, meu nobre')
+        page.dialog = popUp
+        popUp.open = True
         page.update()
 
-    def botaoLogin(e):
-        page.dialog = resposta
-        resposta.open = True
-        page.update()
-    botao = ft.ElevatedButton('Clique aqui',on_click=botaoLogin)
-    
-    
 
+    # Criando campos
+    texto = ft.Text(value="Só um teste bóia", text_align= ft.TextAlign.CENTER, size=_size_text)
+    login = ft.TextField(label='Insira seu login', width=_with_input,text_align=ft.TextAlign.CENTER,on_submit=realizarLogin)
+    senha = ft.TextField(label='Insira sua senha',password=True, width=_with_input,text_align=ft.TextAlign.CENTER,on_submit=realizarLogin)
+    botao_login = ft.ElevatedButton('Entrar',on_click=realizarLogin, width=_with_button, height=_height_button)
+    esqueci_senha = ft.TextButton('Esqueci minha senha',on_click=esqueciSenha)
+
+    # Criando popup
+    popUp = ft.AlertDialog(on_dismiss='')
+
+    # Inserindo os campos na página
     coluna.controls.append(texto)
     coluna.controls.append(login)
     coluna.controls.append(senha)
-    coluna.controls.append(botao)
+    coluna.controls.append(esqueci_senha)
+    coluna.controls.append(botao_login)
     page.controls.append(coluna)
     
     page.update()
-
-    
-
-    
 
 ft.app(target=main, view=ft.WEB_BROWSER, port=8000)
